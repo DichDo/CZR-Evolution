bd48bef99b32f4025d36a62e4f9750fcae360b98
Dichido <220124673+DichDo@users.noreply.github.com>
2025-10-14 01:12:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', title='CZR Barbour Evolution')
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
